03b4a7c5691d1d0644ffb396d19c58065ff07ed7
Emmanuel SA <a20190702@pucp.edu.pe>
2023-03-31 18:07:29 -0500

    Board Messaging
    - Board Tiles sinchronization
    - Simple Turns system added

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -1,16 +1,49 @@
 using Unity.Netcode;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.UIElements;
 
 public class Board : NetworkBehaviour
 {
     [SerializeField] private Box[][] boxes;
+    private bool isCrossPlayer = false;
+    private bool isPlayerTurn = false;
 
     public Box[][] Boxes => boxes;
-
-    private void Awake()
+    public List<Box> BoxList
     {
-        boxes = new Box[3][];
+        get
+        {
+            return new List<Box>(boxes[0].Concat(boxes[1]).Concat(boxes[2]));
+        }
+        
+    }
+
+    private void Start()
+    {
+        boxes = LoadBoxes();
+        NetworkManager.Singleton.OnServerStarted += () =>
+        {
+            isCrossPlayer = true;
+            isPlayerTurn = true;
+        };
+
+        List<Box> list = BoxList;
+        for (int i = 0; i < list.Count; i++)
+        {
+            Box box = list[i];
+            int index = i;
+            box.Button.onClick.AddListener(() =>
+            {
+                SendMovementServerRpc(index, NetworkManager.Singleton.LocalClientId);                
+            });
+        }
+    }
+
+    private Box[][] LoadBoxes ()
+    {
+        Box[][] boxes = new Box[3][];
         boxes[0] = new Box[3];
         boxes[1] = new Box[3];
         boxes[2] = new Box[3];
@@ -19,30 +52,49 @@ public class Board : NetworkBehaviour
         {
             Box box = transform.GetChild(i).GetComponent<Box>();
             boxes[i / 3][i % 3] = box;
-            if (IsServer)
+        }
+        return boxes;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SendMovementServerRpc (int position, ulong clientId)
+    {
+        if (IsMovementValid(position))
+        {
+            if (clientId != NetworkManager.Singleton.LocalClientId)
             {
-                Box.BoxContent[] contents = GetContents();
-                UpdateBoardClientRpc(contents);
+                
+                if (!isPlayerTurn)
+                {
+                    if (isCrossPlayer)
+                        SetBoxContentClientRpc(position, Box.BoxContent.Circle);
+                    else
+                        SetBoxContentClientRpc(position, Box.BoxContent.Cross);
+                    isPlayerTurn = true;
+                }
+            }
+            else
+            {
+                if (isPlayerTurn)
+                {
+                    if (isCrossPlayer)
+                        SetBoxContentClientRpc(position, Box.BoxContent.Cross);
+                    else
+                        SetBoxContentClientRpc(position, Box.BoxContent.Circle);
+                    isPlayerTurn = false;
+                }
             }
         }
     }
 
     [ClientRpc]
-    private void UpdateBoardClientRpc (Box.BoxContent[] contents)
+    private void SetBoxContentClientRpc (int position, Box.BoxContent content)
     {
-        SetContents(contents);
-        print("Updated received from server!");
+        BoxList[position].Content = content;
     }
 
-    private Box.BoxContent[] GetContents ()
+    private bool IsMovementValid (int position)
     {
-        Box.BoxContent[] contents = new Box.BoxContent[9];
-        
-        return contents;
-    }
-
-    private void SetContents (Box.BoxContent[] contents)
-    {
-
+        return true;
     }
 }
---
+++ README.md
@@ -0,0 +1,23 @@
+0. Pre Condition
+	0.1 Both client and server scenes are loaded
+1. Connection
+	1.1 Server Side Starts (PERMA SERVER PC)
+	1.2 Client Connects and game can start
+2. Gameplay
+	2.1 Server Has Cross (Cross is always first)
+	2.2 Game Starts
+		2.2.1 Cross Makes move
+		2.2.2 Check Game over (Winner or no more moves)
+		2.2.3 Circle Makes move
+		2.2.4 Check Game over
+		2.2.5 Go back to 2.2.1
+	2.3 Game is over
+		2.3.1 Roles are switched
+		2.3.2 Board is cleaned
+		2.3.3 Go back to 2.2
+
+EXTRAS
+- Nickname for players
+- Points per won game for each player
+- Local Game
+- Online Game
